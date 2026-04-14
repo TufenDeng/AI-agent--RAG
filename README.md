4fe319ec4b6326d66e0d61a8135d2d57165dab89
TufenDeng <2094594065qq@gmail.com>
2026-04-15 02:38:36 +0800

    feat: full RAG pipeline with local embeddings and deepseek integration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+项目简介：
+     这是一个基于RAG技术开发的算法ai指导师，参考文献为OI_WIKI
+     技术栈为python langchain embedding 
+
 4.12 1.配置搭建RAG的虚拟环境，并从镜像站下载longChain，测试其能正常使用
 
      2.理解什么叫做RAG（检索增强生成），解决ai幻觉问题
@@ -9,3 +13,15 @@
      
      5.做RAG，定义template（变量）时，变量是需要后面才传进来的（延时填充），不能带f（即时求值）
      LangChain Template：用于编排 AI 逻辑。它允许变量暂不存在，通过 {} 留出空位，等 invoke() 时再统一填入
+
+4.13 1.编写ingest,用以清理数据
+     2.理解DirectoryLoader和TextLoader的功能，并明白如何使用这两个类实例化对象
+     3.理解什么是元数据和块的起始位置
+
+4.14 0.对于昨天的文件切割，不同的文件的处理逻辑不同，应该使用不同的切割工具，分割逻辑应该根据文件的“语义结构”来定
+     今天就是跑通RAG的大脑
+     1.安全开发：使用单独的.env文件存储api
+     2.理解embedding模型和llm模型的调用
+     3.环境攻坚：解决了国内访问 Hugging Face 模型的网络瓶颈（HF_ENDPOINT 镜像设置）
+     4.RAG 逻辑：理清了检索（Similarity Search）、数据清洗（join 拼接）到最终生成（DeepSeek 调用）的全流程
+     *remaining problem:数据库并没有正确地转化近万个知识块，问题待排查
---
+++ ask.py
@@ -0,0 +1,47 @@
+import os
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"#还是需要保持环境的一致性，不然需要获取向量化模型的时候又慢
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+#加载数据库
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+db=Chroma(persist_directory="chroma_db",embedding_function=embeddings)#问题本身也需要被转化为向量，这个转化过程用到的模型一致
+
+#接入deepseek
+llm=ChatOpenAI(
+    model="deepseek-chat",
+    openai_api_key=os.getenv("DEEPSEEK_API_KEY"),
+    openai_api_base='https://api.deepseek.com/v1'
+)
+
+#设计教练风格，使用这样的格式可以一眼看清楚给ai下的指令逻辑
+template="""
+你是一个严谨的ccpc算法教练，
+请根据以下参考资料回答问题。如果资料里没写，就说你不知道，不要瞎编
+参考资料：
+{context}
+
+用户问题：
+{question}
+"""
+
+prompt_template = ChatPromptTemplate.from_template(template)
+
+query = "Dijkstra 算法的时间复杂度是多少？"
+
+print(f"\n> 正在本地数据库中搜寻关于 '{query}' 的资料...")
+# 检索最相关的 3 个片段
+docs = db.similarity_search(query, k=3)
+context_text = "\n\n---\n\n".join([doc.page_content for doc in docs])#这是给ai看的，ai看不懂python给的列表，必须是纯字符；join是把这三个文本使用\n---\n连接起来
+
+print("> 资料已找到，正在请 DeepSeek 教练总结并回答...\n")
+full_prompt = prompt_template.format(context=context_text, question=query)#这时候才传参
+response = llm.invoke(full_prompt)
+
+print("=== 教练的回答 ===")
+print(response.content)
