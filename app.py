a317f745b345448d953a7bc24c80eea846bcc24a
TufenDeng <2094594065qq@gmail.com>
2026-04-17 03:07:05 +0800

    fix:change version of chroma

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 #目前全球最火的、专为 机器学习和 AI 开发者 准备的开源 Web 应用框架
 
 #为了使app能在云端运行
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass 
 
 import streamlit as st#缩写
 import os
